879f03aa9d00a7ce7cd57ce14899834daf4d2199
Shadrack Lilan <shadrack.lilan@paygoenergy.co>
2021-02-19 16:04:08 +0300

    add detox
    This only works for debug builds

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
+  env: {
+    jest: true
+  },
+  globals: {
+    element: false,
+    device: false,
+    by: false,
+  },
   extends: '@react-native-community',
 };
--- App.js
+++ App.js
@@ -28,7 +28,7 @@ const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+      <SafeAreaView testID="welcome">
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
